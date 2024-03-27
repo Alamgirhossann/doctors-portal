d3010c8dceb0ebdf60301b164abe4d953bf0e069
Alamgirhossann <alamgirhossann@gmail.com>
2024-03-27 16:47:22 +0600

    text changes

--- src/Components/Appointment/BookingCard/BookingCard.js
+++ src/Components/Appointment/BookingCard/BookingCard.js
@@ -1,29 +1,35 @@
-import React, { useState } from 'react';
-import BookingForm from '../BookingForm/BookingForm';
+import React, { useState } from "react";
+import BookingForm from "../BookingForm/BookingForm";
 
-const BookingCard = ({book, date}) => {
-    const [modalIsOpen, setIsOpen] = useState(false);
-    function openModal() {
-        setIsOpen(true);
-    }
+const BookingCard = ({ book, date }) => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
 
-    function closeModal() {
-        setIsOpen(false);
-    }
-    return (
-        <div className='col-md-4 mb-5'>
-            <div className="card p-3">
-                <div className="card-body text-center">
-                    <h5 className='card-title text-brand'>{book.subject}</h5>
-                    <h6>{book.visitingHour}</h6>
-                    <p>{book.totalSpace} Space Available</p>
-                    <button onClick={openModal} className='text-white btn btn-brand'>BOOK APPOINTMENT</button>
-                    <BookingForm modalIsOpen={modalIsOpen} closeModal={closeModal} date={date} bookingOn={book.subject}></BookingForm>
-                </div>
-            </div>
-            
+  function closeModal() {
+    setIsOpen(false);
+  }
+  return (
+    <div className="col-md-4 mb-5">
+      <div className="card p-3">
+        <div className="card-body text-center">
+          <h5 className="card-title text-brand">{book.subject}</h5>
+          <h6>{book.visitingHour}</h6>
+          <p>{book.totalSpace} Space Available</p>
+          <button onClick={openModal} className="text-white btn btn-brand">
+            BOOK APPOINTMENTT
+          </button>
+          <BookingForm
+            modalIsOpen={modalIsOpen}
+            closeModal={closeModal}
+            date={date}
+            bookingOn={book.subject}
+          ></BookingForm>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default BookingCard;
